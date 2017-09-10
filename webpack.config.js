04a60afe4951398fa1700234fa5c8bd9cd23246b
AnastasiaGritsuk <natikana_@mail.ru>
2017-09-10 12:47:50 +0300

    send html page

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
     output: {
         filename: 'bundle.js',
-        path:  path.resolve(__dirname, 'built'),
+        path:  path.resolve(__dirname, 'build'),
     },
 
     resolve: {
