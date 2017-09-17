26fa905d66e0a79606fc87da97938b50fba5e4f5
AnastasiaGritsuk <natikana_@mail.ru>
2017-09-17 22:00:46 +0300

    decompose high level

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import s from './styles/main.css';
+import {Search} from "./components/Search";
+import {Results} from "./components/Results";
+import {Footer} from "./components/Footer";
 
-const Footer = ()=> {
-    return <div>footer</div>
-};
-
-const Results = ()=> {
-    return <div className={s.btn}>results</div>
-};
-
-const Header = ()=> {
-    return <div>header</div>
-};
-
-const App = ()=> {
-  return <div>
-      <Header/>
-      <Results/>
-      <Footer/>
-    </div>
-};
+class App extends React.Component {
+    render() {
+       return <div id={s.container}>
+           <Search/>
+           <Results/>
+           <Footer/>
+       </div>
+    }
+}
 
 ReactDom.render(
     <App/>,
---
+++ src/components/Search.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export class Search extends React.Component {
+    render() {
+        return <div>header</div>
+    }
+}
