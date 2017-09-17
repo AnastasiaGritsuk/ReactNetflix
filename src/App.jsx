d454a226d796200d3c743ab9a1d085db6a7a6afd
AnastasiaGritsuk <natikana_@mail.ru>
2017-09-17 21:29:47 +0300

    first attempt create structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import s from './styles/main.css';
 
+const Footer = ()=> {
+    return <div>footer</div>
+};
+
+const Results = ()=> {
+    return <div className={s.btn}>results</div>
+};
+
+const Header = ()=> {
+    return <div>header</div>
+};
+
+const App = ()=> {
+  return <div>
+      <Header/>
+      <Results/>
+      <Footer/>
+    </div>
+};
+
 ReactDom.render(
-    <div id="btn" className={s.btn}>Hello world</div>,
+    <App/>,
     document.getElementById("root")
 );
-
-console.log('4444');
