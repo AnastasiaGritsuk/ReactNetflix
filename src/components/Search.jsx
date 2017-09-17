4cbd0f746a9fd0b146ddeba2b64013107b2d7e71
AnastasiaGritsuk <natikana_@mail.ru>
2017-09-17 22:56:22 +0300

    styles for search input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,6 +6,14 @@ export class Search extends React.Component {
     render() {
         return <div className={s.searchWrapper}>
             <h4 className={m.textRed}>netflixroulette</h4>
+
+            <form action="#" method="post" className={m.searchForm}>
+                <h2 className={m.h2}>Find your movie</h2>
+                <input type="text" className={s.searchFormInput} />
+
+            </form>
+
+
         </div>
     }
 }
