5ca7c7ec394cf58a1b7100f58486a8ffc7530517
AnastasiaGritsuk <natikana_@mail.ru>
2017-09-18 00:34:11 +0300

    add item and emptyresultsbody

---
+++ src/components/EmptyResultsBody.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export class EmptyResultsBody extends React.Component {
+    render() {
+        return <div>
+            <h1>No films found!</h1>
+        </div>
+    }
+}
--- src/components/ResultsBody.jsx
+++ src/components/ResultsBody.jsx
@@ -1,9 +1,13 @@
 import * as React from "react";
+import {Item} from "./Item";
 
 export class ResultsBody extends React.Component {
     render() {
         return <div>
-
+            <ul>
+                <Item/>
+                <Item/>
+            </ul>
         </div>
     }
 }
