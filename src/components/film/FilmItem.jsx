0bf006ac35ab6a6091b03cbf6f73dc4810870604
AnastasiaGritsuk <natikana_@mail.ru>
2017-09-18 10:02:06 +0300

    styles for film

--- src/components/film/FilmItem.jsx
+++ src/components/film/FilmItem.jsx
@@ -5,12 +5,12 @@ export class Item extends React.Component {
     render() {
         return <div className={style.item}>
             <img src="" className={style.itemImg} alt=""/>
-            <div className={style.itemCaption}>
-                <div>
+            <div className={style.itemCaptionWrap}>
+                <div className={style.itemCaption}>
                     <h4 className={style.itemTitle}>Kill Bill</h4>
-                    <span>2012</span>
+                    <span className={style.itemTitleYear}>2012</span>
                 </div>
-                <p>Dramas</p>
+                <p className={style.textNote}>Dramas</p>
             </div>
         </div>
     }
