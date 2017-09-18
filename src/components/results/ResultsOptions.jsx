dddd7f1c6f1aed58b3897b7d0c925b1161451722
AnastasiaGritsuk <natikana_@mail.ru>
2017-09-18 10:11:21 +0300

    styles search options

--- src/components/results/ResultsOptions.jsx
+++ src/components/results/ResultsOptions.jsx
@@ -1,18 +1,18 @@
 import * as React from "react";
-import * as m from '../../styles/main.css';
+import * as style from '../../styles/main.css';
 
 export class ResultsOptions extends React.Component {
     render() {
-        return <div className={m.resultsOptionsWrapper}>
-            <div className={m.resultsOptions}>
+        return <div className={style.resultsOptionsWrapper}>
+            <div className={style.resultsOptions}>
                 <div>
                     <span id="resultsCount">7</span>
                     <span> movies found</span>
                 </div>
-                <div>
-                    <span>Sort by </span>
-                    <button type="button">release date</button>
-                    <button type="button">rating</button>
+                <div className={style.resultsFilter}>
+                    <span className={[style.textBold, style.resultsFilterText].join(' ')}>Sort by</span>
+                    <button type="button" className={style.btnFilter}>release date</button>
+                    <button type="button" className={[style.btnFilter, style.btnFilterPrimary].join(' ')}>rating</button>
                 </div>
             </div>
         </div>
