288ac1d23057d43341f79c83e7eac5fbf7f48117
AnastasiaGritsuk <natikana_@mail.ru>
2017-10-31 23:59:24 +0300

    add add_todo case [task 4]

--- src/components/todos/reducers.js
+++ src/components/todos/reducers.js
@@ -1,4 +1,4 @@
-import { VisibilityFilters } from './actions'
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
 
 const initialState = {
     visibilityFilter: VisibilityFilters.SHOW_ALL,
@@ -6,5 +6,23 @@ const initialState = {
 };
 
 function todoApp(state = initialState, action) {
+    switch (action.typeof) {
+        case SET_VISIBILITY_FILTER:
+            return Object.assign({}, state, {
+                visibilityFilter: action.filter
+            });
+        case ADD_TODO:
+            return Object.assign({}, state, {
+                todos: [
+                    ...state.todos,
+                    {
+                        text: action.text,
+                        completed: false
+                    }
+                ]
+            });
+        default:
+            return state;
+    }
     return state;
 }
