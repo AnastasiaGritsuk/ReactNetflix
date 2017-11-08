5055074ad834ef2cc3a9952c5ce2543d53987ff9
Anastasiya Hrytsuk <Anastasiya_Hrytsuk@epam.com>
2017-11-08 12:36:08 +0300

    update filmitem with props task4

--- src/components/film/FilmItem.jsx
+++ src/components/film/FilmItem.jsx
@@ -10,11 +10,11 @@ export class Item extends React.Component {
                 <div className={style.itemCaptionWrap}>
                     <div className={style.itemCaption}>
                         <a className={style.itemTitle}>
-                            <Link to="/film">Kill Bill</Link>
+                            <Link to="/film">{this.props.film.title}}</Link>
                         </a>
-                        <span className={style.itemTitleYear}>2012</span>
+                        <span className={style.itemTitleYear}>{this.props.film.releaseYear}</span>
                     </div>
-                    <p className={style.textNote}>Dramas</p>
+                    <p className={style.textNote}>{this.props.film.genre}</p>
                 </div>
             </div>
         );
