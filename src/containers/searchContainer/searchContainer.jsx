d8ecea1eeda6e4de1a81f63d3d8a8a03f5538e00
Anastasiya Hrytsuk <Anastasiya_Hrytsuk@epam.com>
2017-11-13 14:43:17 +0300

    searchContainer initial

--- src/containers/searchContainer/searchContainer.jsx
+++ src/containers/searchContainer/searchContainer.jsx
@@ -1,29 +1,23 @@
 import * as React from "react";
 import * as style from '../../styles/main.css';
 import Page from '../../components/App'
-import SearchForm from '../../components/search/SearchForm'
-import ResuultsOptions from '../../components/results/ResuultsOptions'
-import ResuultsBody from '../../components/results/ResuultsOptions'
+import SearchForm from './SearchForm/SearchForm'
+import SearchResultsOptions from './SearchResultsOptions/SearchResultsOptions'
+import SearchResults from './SearchResults/SearchResults'
 
 export class Item extends React.Component {
     render() {
         return (
             <Page>
                 <SearchForm
-                    sources={SOURCES}
-                    changeSource={this.changeSource}
                     selectedValue={this.props.page.source}
-                    search={this.changeQuery}
                     query={this.props.page.query}
                 />
-                <ResuultsOptions
-                    moviesAmount={this.props.page.movies.length}
-                    selectedValue={this.props.page.sortBy}
-                    options={SORTS}
-                    changeSort={this.changeSort}
+                <SearchResultsOptions
+                    
                 />
-                <ResuultsBody
-                    movies={this.props.page.movies}
+                <SearchResults
+                    films={this.props.page.movies}
                     history={this.props.history}
                 />
             </Page>
